59471a455cc5c83afb0728c9a4bc4a9cf7fc6434
Aidan Border <planes4u@outlook.com>
2024-03-15 16:36:33 -0400

    Adding method stubs to clear compile errors for now

---
+++ src/main/java/com/bee/MyScheduler.java
@@ -0,0 +1,21 @@
+package com.bee;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class MyScheduler {
+
+    public MyScheduler(int numJobs, String property) {
+        //TODO Auto-generated constructor stub
+    }
+
+    public void run() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+
+    public LinkedBlockingQueue<Job> getOutgoingQueue() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getOutgoingQueue'");
+    }
+
+}
