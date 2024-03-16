ff0e6c62046c7b569dc4c49a535a2e0aa72486a4
Aidan Border <planes4u@outlook.com>
2024-03-16 11:44:30 -0400

    Implementing constructor, getOutgoingQueue, getIncomingQueue, and adding comments
    Also added some fields

--- src/main/java/com/bee/MyScheduler.java
+++ src/main/java/com/bee/MyScheduler.java
@@ -2,20 +2,48 @@ package com.bee;
 
 import java.util.concurrent.LinkedBlockingQueue;
 
+/*
+ *
+ * Models a thread-safe, non-preemptive Scheduler.
+ *
+ * @author Aidan Border
+ * @author Austin Scheetz
+ *
+ */
 public class MyScheduler {
+    private String property; // The parameter we're measuring during this test run
+    private LinkedBlockingQueue<Job> incomingQueue; // The queue of jobs that the scheduler needs to work on.
+    private LinkedBlockingQueue<Job> outgoingQueue; // The queue housing jobs we've already worked on and completed.
 
+    /**
+     * @param numJobs
+     * @param property
+     */
     public MyScheduler(int numJobs, String property) {
-        //TODO Auto-generated constructor stub
+        this.property = property;
+        this.incomingQueue = new LinkedBlockingQueue<>();
+        this.outgoingQueue = new LinkedBlockingQueue<>();
     }
 
+    /**
+     * This is our main method for the Scheduler. All jobs that come in will eventually have this method run in order to give them CPU time and all that. 
+     */
     public void run() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'run'");
     }
 
+    /**
+     * @return LinkedBlockingQueue<Job> outgoingQueue
+     */
     public LinkedBlockingQueue<Job> getOutgoingQueue() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getOutgoingQueue'");
+        return outgoingQueue;
     }
 
+    /**
+     * @return LinkedBlockingQueue<Job> incomingQueue
+     */
+    public LinkedBlockingQueue<Job> getIncomingQueue() {
+        return incomingQueue;
+    }
 }
