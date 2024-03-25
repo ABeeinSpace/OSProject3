20e930d04a970d3a46316796269e59dc458b6e25
Aidan Border <planes4u@outlook.com>
2024-03-25 13:51:53 -0400

    Running autoformat

--- src/main/java/com/bee/MyScheduler.java
+++ src/main/java/com/bee/MyScheduler.java
@@ -16,28 +16,31 @@ import java.util.concurrent.Semaphore;
 public class MyScheduler {
     private String property; // The parameter we're measuring during this test run
     private LinkedBlockingQueue<Job> incomingQueue; // The queue of jobs that the scheduler needs to work on.
-    private LinkedBlockingQueue<Job> outgoingQueue; // The queue housing jobs we've already worked on and completed.
+    private LinkedBlockingQueue<Job> outgoingQueue; // The queue housing jobs we've already worked on and
+                                                    // completed.
     private Semaphore locker;
 
     /**
-     * @param numJobs The number of jobs we're going to use for this run
+     * @param numJobs  The number of jobs we're going to use for this run
      * @param property The property/attribute to maximize for this run
      */
     public MyScheduler(int numJobs, String property) {
         this.property = property;
         this.incomingQueue = new LinkedBlockingQueue<>();
         this.outgoingQueue = new LinkedBlockingQueue<>();
-        this.locker = new Semaphore(numJobs/2);
+        this.locker = new Semaphore(numJobs / 2);
     }
 
     /**
-     * This is our main method for the Scheduler. All jobs that come in will eventually have this method run in order to give them CPU time and all that. 
+     * This is our main method for the Scheduler. All jobs that come in will
+     * eventually have this method run in order to give them CPU time and all
+     * that.
      */
     public void run() {
         // TODO Create Filter based Off Property
         ArrayList<Job> inbetweener = new ArrayList<>();
         switch (this.property) {
-            case "max wait":    
+            case "max wait":
                 try {
                     this.locker.acquire();
                     inbetweener.add(this.incomingQueue.take());
@@ -47,8 +50,13 @@ public class MyScheduler {
                 }
                 this.outgoingQueue.add(inbetweener.remove(0));
                 break;
-            
+
             case "avg wait":
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    // TODO: handle exception
+                }
                 Job shortestJob = incomingQueue.peek();
                 long shortestWait = shortestJob.getLength();
                 for (Job nextJob : incomingQueue) {
@@ -61,7 +69,7 @@ public class MyScheduler {
                 inbetweener.add(shortestJob);
                 this.outgoingQueue.add(inbetweener.remove(0));
                 break;
-            
+
             case "combined":
                 if (incomingQueue.size() > 1) {
                     // Use FCFS if there are multiple jobs in the queue to be processed
@@ -73,7 +81,8 @@ public class MyScheduler {
                         System.err.println("Failed to take from Incoming Queue!!!");
                     }
                 } else {
-                    // Use SJF if there are multiple jobs in the queue waiting to be processed
+                    // Use SJF if there are multiple jobs in the queue waiting to be
+                    // processed
                     Job shortestCombinedJob = incomingQueue.peek();
                     long shortestCombinedWait = shortestCombinedJob.getLength();
                     for (Job incomingJob : incomingQueue) {
@@ -86,8 +95,7 @@ public class MyScheduler {
                     inbetweener.add(shortestCombinedJob);
                     this.outgoingQueue.add(inbetweener.remove(0));
                 }
-                break;        
-                
+                break;
 
             case "deadlines":
                 Job shortestDeadline = incomingQueue.peek();
@@ -118,7 +126,8 @@ public class MyScheduler {
 
     @Override
     public String toString() {
-        return "MyScheduler [property=" + property + ", incomingQueue=" + incomingQueue + ", outgoingQueue="
-                + outgoingQueue + "]";
+        return "MyScheduler [property=" + property +
+                ", incomingQueue=" + incomingQueue +
+                ", outgoingQueue=" + outgoingQueue + "]";
     }
 }
